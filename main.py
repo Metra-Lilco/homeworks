298a06efa36f17b73f53d9f9bba831d881f5c40c
Lilco <metra.lilco@gmail.com>
2023-09-16 21:23:20 +0300

    Add 6 tests

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('asset=Bitcoin;amount=2;') == {'asset': 'Bitcoin', 'amount': '2'}
+    assert parse_cookie('asset=Bitcoin;transaction=True;') == {'asset': 'Bitcoin', 'transaction': 'True'}
+    assert parse_cookie('asset=Bitcoin;stop_loss_price=24000;') == {'asset': 'Bitcoin', 'stop_loss_price': '24000'}
+    assert parse_cookie('active=True;') == {'active': 'True'}
+    assert parse_cookie('name=John;') == {'name': 'John'}
+    assert parse_cookie('name=John;age=30;') == {'name': 'John', 'age': '30'}
